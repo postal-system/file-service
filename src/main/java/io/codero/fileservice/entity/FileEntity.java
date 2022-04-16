d73ae686a36d1efa1f3f106e52caeb788d3455bb
asmirnov <smirnov141188@gmail.com>
2022-04-16 15:28:31 +0600

    PEJT-32: fix import. without asterisk

--- src/main/java/io/codero/fileservice/entity/FileEntity.java
+++ src/main/java/io/codero/fileservice/entity/FileEntity.java
@@ -5,7 +5,13 @@ import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.Type;
 
-import javax.persistence.*;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Lob;
+import javax.persistence.Table;
+import java.time.LocalDateTime;
 import java.util.UUID;
 
 @Data
